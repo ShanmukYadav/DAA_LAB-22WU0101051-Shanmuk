d884c5aaeaf35072aa5ed32adbafe56944120592
Shanmuk <130898922+ShanmukYadav@users.noreply.github.com>
2023-11-12 22:48:13 +0530

    Update Leader Array.py

--- Leader Array.py
+++ Leader Array.py
@@ -2,18 +2,11 @@ def find_leaders(arr):
     n = len(arr)
     leaders = [arr[n - 1]]  # last element is always a leader
     max_from_right = arr[n - 1]
-
-    # Scan the array from right to left
     for i in range(n - 2, -1, -1):
         if arr[i] >= max_from_right:
             max_from_right = arr[i]
             leaders.append(max_from_right)
-
-    # Reverse the list of leaders as we scanned from right to left
     leaders = leaders[::-1]
-
     return leaders
-
-# Test the function
 arr = [16, 17, 4, 3, 5, 2]
 print(find_leaders(arr))
